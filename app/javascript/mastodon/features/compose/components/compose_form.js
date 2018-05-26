829e78edfbfadc3740c4a8a766c9a54fb6de6ec5
Shad Faust <38153806+myrrhashad@users.noreply.github.com>
2018-05-26 19:05:43 +0200

    Max character toots 3rd attempt

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -206,11 +206,11 @@ export default class ComposeForm extends ImmutablePureComponent {
             <SensitiveButtonContainer />
             <SpoilerButtonContainer />
           </div>
-          <div className='character-counter__wrapper'><CharacterCounter max={maxChars} text={text} /></div>
+          <div className='character-counter__wrapper'><CharacterCounter max={750} text={text} /></div>
          </div>
 
           <div className='compose-form__publish'>
-            <div className='compose-form__publish-button-wrapper'><Button text={publishText} onClick={this.handleSubmit} disabled={disabled || this.props.is_uploading || length(text) > maxChars || (text.length !== 0 && text.trim().length === 0)} block /></div>
+            <div className='compose-form__publish-button-wrapper'><Button text={publishText} onClick={this.handleSubmit} disabled={disabled || this.props.is_uploading || length(text) > 750 || (text.length !== 0 && text.trim().length === 0)} block /></div>
           </div>
         </div>
     );
